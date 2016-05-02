c2606c3c50656818e5c9c4e2de41fbc34fb9b784
Dmitry MiksIr <miksir@maker.ru>
2016-05-02 04:00:21 +0300

    Package description fix

--- FakerController.php
+++ FakerController.php
@@ -12,46 +12,6 @@ use yii\helpers\Console;
 
 /**
  * This command can fill your database with fake data.
- *
- * You can switch between different data generators (or write your own) and
- * different database connectors.
- *
- * Generators included:
- * - FakeGenerator: this is proxy to [fzaninotto/faker]
- * and use same logic, paths and templates as [yiisoft/yii2-faker]. Read yii2-faker documenctation about
- * creating fixtures template file
- *
- * Database connectors (called here as dbproviders):
- * - Csv: simple csv writer
- * - ActiveRecord: create model, fill with data and call ->save()
- * - YiiDAO: insert data using sql (via Yii's PDO), can insert multiple rows per one insert
- *
- * Instalation:
- *
- * You should configure your application as follows
- * (usually in console.php; and you can use any alias, not only "faker"):
- *
- * ~~~
- * 'controllerMap' => [
- *     'faker' => [
- *         'class' => 'MiksIr\Yii2DbFaker\FakerController',
- *     ],
- * ],
- * ~~~
- *
- * Call help for current generator and dbprovider
- *
- * ~~~
- * yii faker/help
- * ~~~
- *
- * Examples:
- *
- * ~~~
- * yii faker/generate --count=1000000 --dbprovider=YiiDAO generator_template=users dbprovider_table=users truncate=1
- * ~~~
- * 
- * Truncate table users and create about 1 million rows using template from @tests/unit/templates/fixtures/users.php
  */
 class FakerController extends Controller
 {
--- README.md
+++ README.md
@@ -1,5 +1,43 @@
-# yii2-db-faker
-
 This Yii2 command can fill your database with fake data.
 
-You can install it through [composer](http://getcomposer.org/download/).
+You can switch between different data generators (or write your own) and
+different database connectors.
+
+Generators included:
+- FakeGenerator: this is proxy to [`Faker`](https://github.com/fzaninotto/Faker)
+and use same logic, paths and templates as [`yii2-faker`](https://github.com/yiisoft/yii2-faker) . Read yii2-faker documentation about
+creating fixtures template file
+
+Database connectors (called here as dbproviders):
+- Csv: simple csv writer
+- ActiveRecord: create model, fill with data and call ->save()
+- YiiDAO: insert data using sql (via Yii's PDO), can insert multiple rows per one insert
+
+Installation
+------------
+
+You should configure your application as follows
+(usually in console.php; and you can use any alias, not only "faker"):
+
+```php
+'controllerMap' => [
+ 'faker' => [
+     'class' => 'MiksIr\Yii2DbFaker\FakerController',
+ ],
+],
+```
+
+Call help for current generator and dbprovider
+
+```
+yii faker/help
+```
+
+Examples
+--------
+
+```
+yii faker/generate --count=1000000 --dbprovider=YiiDAO generator_template=users dbprovider_table=users truncate=1
+```
+
+Truncate table users and create about 1 million rows using template from \@tests/unit/templates/fixtures/users.php
